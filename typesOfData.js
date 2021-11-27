9bcec81bf9750d5cb33d3ce56fb7da1706771c42
Alina <panasiukalina0@gmail.com>
2021-11-28 00:35:09 +0300

    types of datas

---
+++ typesOfData.js
@@ -0,0 +1,16 @@
+const number = 100;
+let string = "Hello";
+string = `Hello Alina ${number}`;
+const boolean = true;
+const NULL = null;
+const UNDEFINED = undefined;
+console.log(string);
+
+const symbol = Symbol("plok9uygtfds");
+const bigint = 10n; //bigint
+
+const func = () => {}; //function
+const arr = []; //array
+const obj = {}; //object
+
+// console.log(typeof );
